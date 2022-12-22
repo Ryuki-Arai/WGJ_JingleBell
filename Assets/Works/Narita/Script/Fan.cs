f0fa9591a00bd2b464d1a4ca814e01000ac800e2
naritayuuto <yuuto122narita@i.softbank.jp>
2022-12-22 15:00:59 +0900

    [Add] ItemBaseを蘇生。ゲームマネージャーを呼ぶ処理を追加。

--- Assets/Works/Narita/Script/Fan.cs
+++ Assets/Works/Narita/Script/Fan.cs
@@ -7,6 +7,7 @@ public class Fan : ItemBase
     float _gageScore = 0;
     public override void ItemAction()
     {
+        GameManager.InstanceGM.AddFanValue(_gageScore);
         //GameManagerのゲージ加算関数を呼び、引数に自身が持つ値セット。
     }
 }
